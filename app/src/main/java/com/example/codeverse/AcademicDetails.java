638372b6968d04750b844ee17de33e54cd39c81e
Dhanuja Rehan <dhanujarehan@gmail.com>
2025-05-30 02:20:08 +0530

    .

--- app/src/main/java/com/example/codeverse/AcademicDetails.java
+++ app/src/main/java/com/example/codeverse/AcademicDetails.java
@@ -89,7 +89,7 @@ public class AcademicDetails extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_academic_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_academic_details, container, false);
 
         // Initialize UI components
         initializeViews(view);
