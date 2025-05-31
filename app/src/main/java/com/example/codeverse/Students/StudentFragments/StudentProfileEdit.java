6ba33ec6c142538518ad85ed210457c5d876a7b3
Dhanuja Rehan <dhanujarehan@gmail.com>
2025-06-01 01:51:34 +0530

    ,

--- app/src/main/java/com/example/codeverse/Students/StudentFragments/StudentProfileEdit.java
+++ app/src/main/java/com/example/codeverse/Students/StudentFragments/StudentProfileEdit.java
@@ -1,4 +1,4 @@
-package com.example.codeverse;
+package com.example.codeverse.Students.StudentFragments;
 
 import android.Manifest;
 import android.app.Activity;
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -40,7 +39,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.Locale;
 
 public class StudentProfileEdit extends Fragment {
