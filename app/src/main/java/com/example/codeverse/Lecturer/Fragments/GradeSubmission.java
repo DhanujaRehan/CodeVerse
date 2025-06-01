2a16aba5257640e33c4e5419daad2338160a7173
Sxdeesh <sadeeshweerasekara@gmail.com>
2025-06-01 18:29:08 +0530

    .

--- app/src/main/java/com/example/codeverse/Lecturer/Fragments/GradeSubmission.java
+++ app/src/main/java/com/example/codeverse/Lecturer/Fragments/GradeSubmission.java
@@ -18,7 +18,6 @@ import com.example.codeverse.R;
 public class GradeSubmission extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
