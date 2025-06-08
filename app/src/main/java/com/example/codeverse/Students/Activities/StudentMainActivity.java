1ce06043a72231a5e5f0b503741d1217b2df530b
Dhanuja Rehan <dhanujarehan@gmail.com>
2025-06-08 13:49:18 +0530

    Student Main nav bar eddited

--- app/src/main/java/com/example/codeverse/Students/Activities/StudentMainActivity.java
+++ app/src/main/java/com/example/codeverse/Students/Activities/StudentMainActivity.java
@@ -8,14 +8,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.codeverse.Lecturer.Fragments.GradeSubmission;
-import com.example.codeverse.Staff.StaffFragments.StaffTimeTabePDF;
 import com.example.codeverse.R;
 import com.example.codeverse.StudentHomeFragment;
 import com.example.codeverse.Students.StudentFragments.PaymentScreenFragment;
 import com.example.codeverse.Students.StudentFragments.StudentClass;
 import com.example.codeverse.Students.StudentFragments.StudentProfile;
-import com.example.codeverse.Students.StudentFragments.TimetableDownloadFragment;
 import com.example.codeverse.databinding.ActivityStudentMainBinding;
 
 public class StudentMainActivity extends AppCompatActivity {
@@ -29,14 +26,13 @@ public class StudentMainActivity extends AppCompatActivity {
         binding = ActivityStudentMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.framelayout, new StudentHomeFragment())
                     .commit();
         }
 
-        binding.studentBottomNavBar.setOnItemSelectedListener(item -> {
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemid = item.getItemId();
             if (itemid == R.id.navhome) {
                 getSupportFragmentManager().beginTransaction()
@@ -44,13 +40,19 @@ public class StudentMainActivity extends AppCompatActivity {
                         .commit();
             }
             else if (itemid == R.id.navassignments){
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new StudentProfile()).commit();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.framelayout, new StudentProfile())
+                        .commit();
             }
             else if (itemid == R.id.navschedule){
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new StudentClass()).commit();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.framelayout, new StudentClass())
+                        .commit();
             }
             else if (itemid == R.id.navprofile){
-                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout, new PaymentScreenFragment()).commit();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.framelayout, new PaymentScreenFragment())
+                        .commit();
             }
             return true;
         });
@@ -58,7 +60,7 @@ public class StudentMainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.student_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
             return insets;
         });
     }
