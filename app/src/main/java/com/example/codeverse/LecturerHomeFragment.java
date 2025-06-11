39990f98d2ee56324f332bb9380f17be18bae4f6
Dhanuja Rehan <dhanujarehan@gmail.com>
2025-06-11 13:33:33 +0530

    Errors done

--- app/src/main/java/com/example/codeverse/LecturerHomeFragment.java
+++ app/src/main/java/com/example/codeverse/LecturerHomeFragment.java
@@ -57,7 +57,7 @@ public class LecturerHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_lecturer_home, container, false);
     }
 }
